b7ab9f88e32460cffd2161c899624a5d21c77864
HiddenCoder <31147960+hiddencoder1337@users.noreply.github.com>
2018-03-08 10:58:54 +0700

    Add files via upload

---
+++ ecashell.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	
+	<title>Authenticate - Ghostbin</title>
+	
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/ghostbin-icon120.png" sizes="120x120">
+	<link rel="apple-touch-icon" href="/ghostbin-icon152.png" sizes="152x152">
+	<link rel="apple-touch-icon" href="/ghostbin-icon76.png" sizes="76x76">
+	<link rel="apple-touch-icon" href="/ghostbin-icon72.png" sizes="72x72">
+
+	<link rel="stylesheet" href="/css/lib.min.d251b95d.css" media="all">
+
+	<link rel="stylesheet" href="/css/application.min.b99ea92e.css" media="all">
+
+
+
+	<link rel="stylesheet" href="/css/theme.min.77312fb0.css" media="all">
+
+	<script src="/js/lib.min.d0a383f9.js"></script>
+
+	<script src="/js/application.min.c66fa356.js"></script>
+
+	
+</head>
+<body>
+	<div class="flash-container" id="flash-container">
+		<div class="well" id="flash-template">
+			<p></p>
+		</div>
+	</div>
+
+
+<div class="paste-toolbox">
+	
+<a title="Home" href="/" id="home" class="btn btn-inverse"><i class="icon-home icon-large"></i></a>
+
+	<span class="paste-title">
+		<i class="icon-lock"></i><strong>Paste nfo9js35</strong>
+		<span class="paste-subtitle">Authentication Required</span>
+	</span>
+</div>
+<div class="well">
+<form method="post">
+<p>Paste <strong>nfo9js35</strong> is password-protected.</p>
+
+<div class="control-group">
+<div class="input-prepend phone-expand">
+	<span class="add-on"><i class="icon-lock"> </i></span>
+	<div class="input-wrapper"><input type="password" name="password" autocomplete="off" autofocus="autofocus"></div>
+</div>
+
+</div>
+<button type="submit" class="btn btn-phone-expand">Okay!</button>
+</form>
+</div>
+
+
+</body>
+</html>
